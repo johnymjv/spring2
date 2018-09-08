15fadda48a120a17d3c139e2b23db96a2951ceac
corre_000 <corre_000@portatil-johny>
2018-09-08 11:16:58 -0500

    Commit con gradle

---
+++ src/main/java/com/bancodebogota/fdsm/ejemplogradle/App.java
@@ -0,0 +1,14 @@
+
+package com.bancodebogota.fdsm.ejemplogradle;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+}
+
